43d4ce1b9b2d8fc2f4048e0bfbd18b027d76b6e8
root <root@vbox.myguest.virtualbox.org>
2025-09-02 14:45:14 -0300

    Other modification

--- debian-initialize.sh
+++ debian-initialize.sh
@@ -6,18 +6,15 @@ YELLOW='\033[0;3
 NC='\033[0m'
 
 if [ -z "$1" ]; then
-	echo -e "${REDIt was not possible run the script! You must provide your user from computer like this: ./debian-initialize.sh your-user${NC}"
+	echo -e "${RED}It was not possible run the script! You must provide your user from computer like this: ./debian-initialize.sh your-user${NC}"
 	exit 1
 fi
 
-if [ "$(id -u)" -eq 0 ]; then
-	echo -e "${RED}Please, don't run this script as root user!${NC}"
+if [ "$(id -u)" -ne 0 ]; then
+	echo -e "${RED}Please, run this script as root user!${NC}"
 	exit 1
 else
 	echo -e "${YELLOW}Initialize script is running!${NC}"
-	echo -e "${YELLOW}Becoming root user...${NC}"
-	su -
-	echo -e "${GREEN}Root with success${NC}"
 	sleep 5s
 
 	# Updating the system
@@ -25,21 +22,21 @@ else
 	echo -e "${YELLOW}Updating and upgrading the system${NC}"
 	apt update -y && apt upgrade -y
 	echo -e "${GREEN}Done!${NC}"
-	sleep 5s
+	sleep 3s
 
 	# Installing sudo
 	clear
 	echo -e "${YELLOW}Installing sudo${NC}"
 	apt install sudo -y
 	echo -e "${GREEN}Done!${NC}"
-	sleep 5s
+	sleep 3s
 
 	# Adding user's machine as sudo
 	clear
 	echo -e "${YELLOW}Setting up sudo to user${NC}"
 	usermod -aG sudo $1
 	echo -e "${GREEN}Done!${NC}"
-	sleep 5s
+	sleep 3s
 
 	# Restarting the machine
 	clear
